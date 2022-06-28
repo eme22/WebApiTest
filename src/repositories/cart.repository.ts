b1ca38e8e31a1f287a1621cee6fde30e8ef0d074
eme22 <48258734+eme22@users.noreply.github.com>
2022-06-28 00:07:15 -0500

    Cart Fix Test 2

--- src/repositories/cart.repository.ts
+++ src/repositories/cart.repository.ts
@@ -52,22 +52,44 @@ export interface ICartPayload {
   
   export const getCart  = async (id: number) :Promise<Cart | null> => {
     const cartRepository = getRepository(Cart);
+    const cartItemsRepository = getRepository(CartItems);
+
+
     const cart = await cartRepository.findOne({id})
+
     if (!cart) return null
+
+    cart.cartItems = await cartItemsRepository.find({cartId: id});
+
     return cart
   }
 
   export const getCartsByUserId  = async (id: number) :Promise<Array<Cart> | null> => {
     const cartRepository = getRepository(Cart);
-    const cart = await cartRepository.find({userId: id})
-    if (!cart) return null
-    return cart
+    const cartItemsRepository = getRepository(CartItems);
+
+    const carts = await cartRepository.find({userId: id})
+    if (!carts) return null
+
+    
+    for (let index = 0; index < carts.length; index++) {
+      const cart = carts[index];
+      cart.cartItems = await cartItemsRepository.find({cartId: cart.id});
+      carts[index]  = cart;
+    }
+
+    return carts
   }
 
   export const getActiveCart  = async (id: number) :Promise<Cart | null> => {
     const cartRepository = getRepository(Cart);
+    const cartItemsRepository = getRepository(CartItems);
+    
     const cart = await cartRepository.findOne({userId: id, finished: false})
     if (!cart) return null
+
+    cart.cartItems = await cartItemsRepository.find({cartId: id});
+
     return cart
   }
 
